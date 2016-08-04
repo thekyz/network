d101f8728ad2a5ff86910a6a8e0699b6b053d5d3
Alexandre Garcia Mayans <alexandre.garcia.mayans.ext@nokia.com>
2016-08-04 16:28:54 +0200

    Simplified code for chatroom and fixed remaining bugs

--- 0_sockets/server.c
+++ 0_sockets/server.c
@@ -5,25 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
 
-#define SERVER_PORT     "6666"
+#define SERVER_PORT     "9877"
 
 #define MAX_CONNECTIONS 16
-
-#define MSG_MAX_LEN     64
 
 #define MSG_IN          "IN"
 #define MSG_OUT         "OUT"
 #define MSG_OK          "OK"
     
-struct client {
-  int socket;
-  char nick[MSG_MAX_LEN];
-  int is_in;
-};
-
-struct client clients_g[MAX_CONNECTIONS] = { 0 };
-
 static int start_server()
 {
   int server_socket;
@@ -65,174 +56,93 @@ static int start_server()
   return server_socket;
 }
 
-struct client *client_in(int server_socket)
+int server_loop(int server_socket)
 {
-  int bytes;
-  int it;
-  int rc;
-  char msg[MSG_MAX_LEN];
-  struct client *client = NULL;
-  int client_socket;
-  struct sockaddr_storage client_address;
-  socklen_t client_address_size;
+  fd_set read_set, master_set;
+  int rc = 0;
+  int it, broadcast_it;
+  int selected = 0;
+  int select_max;
+	char buffer[256];
+
+  FD_ZERO(&read_set);
+  FD_ZERO(&master_set);
 
   rc = listen(server_socket, MAX_CONNECTIONS);
   if (rc < 0) {
     rc = errno;
     fprintf(stderr, "--- listen error: %s\n", strerror(rc));
-    return NULL;
-  }
-
-  client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_size);
-  if (client_socket < 0) {
-    rc = errno;
-    fprintf(stderr, "--- accept error: %s\n", strerror(rc));
-    return NULL;
-  }
-
-  for (it = 0; it < MAX_CONNECTIONS; it++) {
-    if (clients_g[it].is_in == 0) {
-      client = &clients_g[it];
-    }
-  }
-
-  if (client == NULL) {
-    fprintf(stderr, "--- All seats taken, refusing connection.\n");
-    return NULL;
-  }
-
-  client->socket = client_socket;
-
-  bytes = recv(client->socket, msg, sizeof(msg), 0);
-  if (bytes < 0) {
-    fprintf(stderr, "--- recv error: %s\n", strerror(errno));
-    return NULL;
-  }
-
-  if ((strncmp(msg, MSG_IN, strlen(MSG_IN)) != 0) || (strlen(msg) < (strlen(MSG_IN + 2)))) {
-    msg[bytes - 1] = '\0';
-    fprintf(stderr, "--- Received unknown message: %s\n", msg);
-    return NULL;
-  }
-
-  printf("+++ Incoming client: %s\n", msg);
-
-  strcpy(client->nick, &msg[3]);
-  sprintf(msg, "%s", MSG_OK);
-
-  bytes = send(client->socket, msg, strlen(msg) + 1, 0);
-  if (bytes < 0) {
-    fprintf(stderr, "--- send error: %s\n", strerror(errno));
-    return NULL;
-  }
-
-  client->is_in = 1;
-  printf(">>> %s connected !\n", client->nick);
-
-  return client;
-}
-
-int handle_msg(struct client *client)
-{
-  int bytes;
-  char msg[MSG_MAX_LEN];
-
-  bytes = recv(client->socket, msg, sizeof(msg), 0);
-  if (bytes < 0) {
-    fprintf(stderr, "--- recv error: %s\n", strerror(errno));
-    close(client->socket);
     return -1;
   }
 
-  fprintf(stderr, ">>> Received message: %s\n", msg);
-
-  // OUT message
-  if (strncmp(msg, MSG_OUT, strlen(MSG_OUT)) == 0) {
-    sprintf(msg, "%s", MSG_OK);
-    (void)send(client->socket, msg, strlen(msg) + 1, 0);
-    // let's ignore the sent value here, we can't do a lot anyway ...
-    close(client->socket);
-    client->is_in = 0;
-    printf("<<< %s disconnected (%s).\n", client->nick, msg);
-    return 1;
-  }
-
-  // fallback case ...
-  msg[bytes - 1] = '\0';
-  fprintf(stderr, "--- Received unknown message: %s\n", msg);
-  close(client->socket);
-  return -1;
-}
-
-int server_loop(int server_socket)
-{
-  fd_set read_set;
-  struct timeval timeout;
-  int rc = 0;
-  int it;
-  int selected = 0;
-  int select_max;
-
-  FD_ZERO(&read_set);
-  FD_SET(server_socket, &read_set);
-  select_max = server_socket + 1;
-
   printf("+++ Listening on port '%s' ...\n", SERVER_PORT);
 
-  while (1) {
-    timeout.tv_sec = 5;
-    timeout.tv_usec = 0;
+  FD_SET(server_socket, &master_set);
+  select_max = server_socket;
 
-    printf(".\n");
-    selected = select(select_max, &read_set, NULL, NULL, &timeout);
+  for (;;) {
+		memset(buffer, 0, sizeof(buffer));
+    read_set = master_set;
+
+    selected = select(select_max + 1, &read_set, NULL, NULL, NULL);
     if (selected < 0) {
       rc = errno;
       fprintf(stderr, "--- select error: %s\n", strerror(errno));
       return rc;
-    } else if (selected == 0) {
-      // timeout
-      continue;
     }
 
-    if (FD_ISSET(server_socket, &read_set)) {
-      struct client *client = client_in(server_socket);
-      if (client != NULL) {
-        FD_SET(client->socket, &read_set);
+    for (it = 0; it <= select_max; it++) {
+      if (!FD_ISSET(it, &read_set)) {
+					continue;
+			}
 
-        if (client->socket >= select_max) {
-          select_max = client->socket + 1;
-        }
+      if (it == server_socket) {
+				struct sockaddr_storage remote_address;
+				socklen_t address_length = sizeof(remote_address);
+				int client_socket = accept(server_socket, (struct sockaddr *)&remote_address, &address_length);
+				if (client_socket < 0) {
+						fprintf(stderr, "--- accept error: %s\n", strerror(errno));
+						continue;
+				}
 
-        selected--;
-      }
-    }
-    
-    if (selected == 0) {
-      continue;
-    }
+			  FD_SET(client_socket, &master_set);
 
-    for (it = 0; it < MAX_CONNECTIONS; it++) {
-      if (clients_g[it].is_in == 0) {
-        continue;
-      }
+	      if (client_socket > select_max) {
+		      select_max = client_socket;
+	      }
 
-      if (FD_ISSET(clients_g[it].socket, &read_set)) {
-        rc = handle_msg(&clients_g[it]);
-        if (rc != 0) {
-          FD_CLR(clients_g[it].socket, &read_set);
-          clients_g[it].socket = 0;
-        }
+				printf(">>> Client %d connected.\n", client_socket);
+			} else {
+				int bytes = recv(it, buffer, sizeof(buffer), 0);
+				if (bytes < 0) {
+					if (bytes == 0) {
+						// connection closed
+						printf("<<< Client %d disconnected.\n", it);
+					} else {
+						fprintf(stderr, "--- recv error: %s\n", strerror(errno));
+					}
 
-        selected--;
+					close(it);
+					FD_CLR(it, &master_set);
+					continue;
+				}
 
-        if (selected <= 0) {
-          break;
-        }
-      }
-    }
+				// broadcast the received data
+				printf("%d: %s", it, buffer);
+				for (broadcast_it = 0; broadcast_it <= select_max; broadcast_it++) {
+					if (!FD_ISSET(broadcast_it, &master_set)) {
+						continue;
+					}
 
-    if (selected) {
-      fprintf(stderr, "--- Could not find selected socket ...\n");
+					if (broadcast_it == it || broadcast_it == server_socket) {
+						continue;
+					}
+
+					if (send(broadcast_it, buffer, bytes, 0) < -1) {
+						fprintf(stderr, "--- send error: %s\n", strerror(errno));
+					}
+				}
+			}
     }
   };
 
