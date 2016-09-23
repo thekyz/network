8329f0d6e3ac9e710f28cba25084b8cd7e8b76a4
Alexandre Garcia Mayans <alexandre.garcia.mayans.ext@nokia.com>
2016-09-23 11:03:39 +0200

    Added some slides on netcat

--- slides/presentation.sh
+++ slides/presentation.sh
@@ -3,5 +3,5 @@
 echo "Open '127.0.0.1:8000' in a web browser to see the slides ..."
 echo ""
 
-cd "$(dirname "${BASH_SOURCE[0]}")"/slides
+cd "$(dirname "${BASH_SOURCE[0]}")"
 python -m SimpleHTTPServer
