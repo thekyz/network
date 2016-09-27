baef953bde864aa3b22df16c1a5b52e104cb1218
Alexandre Garcia Mayans <alexandre.garcia.mayans.ext@nokia.com>
2016-09-27 14:36:02 +0200

    Updated setup script for zmq

--- 1_zmq/setup.sh
+++ 1_zmq/setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install python-dev -y
+#sudo apt-get update
+#sudo apt-get install python-dev -y
 
-cd
+CWD=$(pwd -P)
+
+mkdir zmq_lib
+cd zmq_lib
+mkdir build
 wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.5/zeromq-4.1.5.tar.gz
 tar xzf zeromq-4.1.5.tar.gz
 cd zeromq-4.1.5
-./configure
+./configure --prefix=${CWD}/zmq_lib
 make
 make install
 
-sudo pip install pyzmq
+#sudo pip install pyzmq
 
