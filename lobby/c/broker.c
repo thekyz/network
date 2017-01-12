3f4a6d2c8115f008226d87bbb37a5a3b8332d749
thekyz <thekyz@gmail.com>
2017-01-12 15:06:29 +0100

    Added clients listing Provisioned for servers listing Added connection/disconnection info from broker to client Fixed a few minor stuff Changed separators to ascii characters to simplify usage of strtok

--- lobby/c/broker.c
+++ lobby/c/broker.c
@@ -4,15 +4,31 @@
 #include <unistd.h>
 #include <signal.h>
 #include <string.h>
+#include <poll.h>
+#include <sys/timerfd.h>
+#include <stdbool.h>
 
 #include <nanomsg/nn.h>
 #include <nanomsg/pipeline.h>
 #include <nanomsg/pubsub.h>
 
 #include "broker.h"
+#include "list.h"
 
-int g_lobby;
-int g_sink;
+static const int g_max_input_length = 256;      // Max number of chars read from the input
+
+// sockets
+static int g_lobby;
+static int g_sink;
+
+struct _connection {
+    list node;
+    char name[BROKER_MAX_NAME_LENGTH];
+    int alive;
+};
+
+static list g_clients;
+static list g_servers;
 
 static int _whisper(const char *from, const char *to, const char *msg)
 {
@@ -24,10 +40,55 @@ static int _broadcast(const char *user, const char *msg)
     return BROKER_SEND(g_lobby, BROKER_MSG_FORMAT(user, msg));
 }
 
+static int _ping()
+{
+    return BROKER_SEND(g_lobby, BROKER_PING_FORMAT(BROKER_PING_BROKER, BROKER_PING_BROKER));
+}
+
+static int _send_connection_list(const char *user, const char *type)
+{
+    char *info_type = NULL;
+    list *conn_list = NULL;
+
+    if (strcmp(type, BROKER_LIST_CLIENTS) == 0) {
+        info_type = BROKER_INFO_CLIENTS;
+        conn_list = &g_clients;
+    } else if (strcmp(type, BROKER_LIST_SERVERS) == 0) {
+        info_type = BROKER_INFO_SERVERS;
+        conn_list = &g_servers;
+    }
+
+    struct _connection *conn;
+    size_t list_msg_size = BROKER_LIST_MAX_SIZE(user, type);
+    char list_msg[list_msg_size];
+    int list_msg_index = 0;
+    list_foreach(conn_list, conn) {
+        if (list_msg_index + strlen(BROKER_UNIT_SEPARATOR) + strlen(conn->name) + 1 > list_msg_size) {
+            // send what we have and prepare the next message
+            BROKER_SEND(g_lobby, BROKER_INFO_FORMAT(user, info_type, list_msg));
+            list_msg_index = 0;
+            list_msg[0] = '\0';
+        }
+
+        list_msg_index += sprintf(&list_msg[list_msg_index], "%s", conn->name);
+
+        if (conn->node.next != conn_list) {
+            list_msg_index += sprintf(&list_msg[list_msg_index], "%s", BROKER_UNIT_SEPARATOR);
+        }
+    }
+
+    return BROKER_SEND(g_lobby, BROKER_INFO_FORMAT(user, info_type, list_msg));
+}
+
 static void _cleanup()
 {
+    BROKER_SEND(g_lobby, BROKER_SHUTDOWN_FORMAT(BROKER_NAME));
+
     nn_shutdown(g_sink, 0);
     nn_shutdown(g_lobby, 0);
+
+    unlink(BROKER_SINK_FILE);
+    unlink(BROKER_LOBBY_FILE);
 
     exit(0);
 }
@@ -37,33 +98,159 @@ static void _int_handler(int dummy)
     _cleanup();
 }
 
+static void _hearthbeat(const char *conn_type, const char *name)
+{
+    list *conn_list = NULL;
+    if (strcmp(conn_type, BROKER_PING_SERVER) == 0) {
+        conn_list = &g_servers;
+    } else if (strcmp(conn_type, BROKER_PING_CLIENT) == 0) {
+        conn_list = &g_clients;
+    }
+
+    struct _connection *conn;
+    list_foreach(conn_list, conn) {
+        if (strcmp(conn->name, name) == 0) {
+            // found it: keep alive
+            conn->alive = 2;
+            return;
+        }
+    }
+
+    conn = (struct _connection *)malloc(sizeof(struct _connection));
+    sprintf(conn->name, "%s", name);
+    conn->alive = 2;
+    list_add_tail(conn_list, &conn->node);
+}
+
+static void _check_connections(list *conn_list)
+{
+    struct _connection *conn;
+    list_foreach(conn_list, conn) {
+        conn->alive--;
+
+        if (conn->alive == 0) {
+            list_delete(&conn->node);
+            free(conn);
+        }
+    }
+}
+
 static void _read_from_sink()
 {
     char *data = NULL;
     int bytes = nn_recv(g_sink, &data, NN_MSG, 0);
     assert(bytes >= 0);
 
-    /*printf("'%s'\n", data);*/
+    /*if (strncmp(strstr(data, BROKER_RECORD_SEPARATOR) + strlen(BROKER_RECORD_SEPARATOR), BROKER_PING, 4) != 0) printf("'%s'\n", data);*/
 
-    char *user = strtok(data, BROKER_UNIT_SEPARATOR);
-    char *cmd = strtok(NULL, BROKER_UNIT_SEPARATOR);
+    char *user = BROKER_FIRST_TOKEN(data);
+    char *cmd = BROKER_NEXT_TOKEN();
 
-    if (strcmp(cmd, BROKER_MSG) == 0) {
-        char *msg = strtok(NULL, BROKER_UNIT_SEPARATOR);
+    if (strcmp(cmd, BROKER_PING) == 0) {
+        char *user_type = BROKER_NEXT_TOKEN();
+        _hearthbeat(user_type, user);
+    } else if (strcmp(cmd, BROKER_MSG) == 0) {
+        char *msg = BROKER_NEXT_TOKEN();
         printf("[B] %s: '%s'\n", user, msg);
         _broadcast(user, msg);
     } else if (strcmp(cmd, BROKER_WHISP) == 0) {
-        char *dest = strtok(NULL, BROKER_UNIT_SEPARATOR); 
-        char *msg = strtok(NULL, BROKER_UNIT_SEPARATOR); 
+        char *dest = BROKER_NEXT_TOKEN();
+        char *msg = BROKER_NEXT_TOKEN();
         _whisper(user, dest, msg);
+    } else if (strcmp(cmd, BROKER_LIST) == 0) {
+        char *type = BROKER_NEXT_TOKEN();
+        _send_connection_list(user, type);
     }
 
     nn_freemsg(data);
 }
 
+static int _poll()
+{
+    struct pollfd nodes[4];
+
+    // stdin, provisioned, not used yet
+    nodes[0].fd = 0;//STDIN_FILENO;
+    nodes[0].events = POLLIN;
+
+    // broker sink
+    size_t fd_size = sizeof(nodes[1].fd);
+    assert(nn_getsockopt(g_sink, NN_SOL_SOCKET, NN_RCVFD, &nodes[1].fd, &fd_size) == 0);
+    nodes[1].events = POLLIN;
+
+    // keep alive
+    struct itimerspec keep_alive_ts;
+    nodes[2].fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+    nodes[2].events = POLLIN;
+    assert(nodes[2].fd != -1);
+
+    keep_alive_ts.it_interval.tv_sec = BROKER_KEEP_ALIVE_PERIOD;
+    keep_alive_ts.it_interval.tv_nsec = 0;
+    keep_alive_ts.it_value.tv_sec = 0;
+    keep_alive_ts.it_value.tv_nsec = 1;
+    assert(timerfd_settime(nodes[2].fd, 0, &keep_alive_ts, NULL) == 0);
+
+    // hearthbeat
+    struct itimerspec hearthbeat_ts;
+    nodes[3].fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+    nodes[3].events = POLLIN;
+    assert(nodes[3].fd != -1);
+
+    hearthbeat_ts.it_interval.tv_sec = BROKER_HEARTHBEAT_PERIOD;
+    hearthbeat_ts.it_interval.tv_nsec = 0;
+    hearthbeat_ts.it_value.tv_sec = 0;
+    hearthbeat_ts.it_value.tv_nsec = 1;
+    assert(timerfd_settime(nodes[3].fd, 0, &hearthbeat_ts, NULL) == 0);
+
+    for (;;) {
+        char input_buffer[g_max_input_length];
+        int rc = poll(nodes, sizeof(nodes) / sizeof(struct pollfd), -1);
+
+        if (rc == -1) {
+            fprintf(stderr, "[C] poll() error: %s\n", strerror(errno));
+            return -1;
+        } else if (rc == 0) {
+            // timeout
+            continue;
+        }
+
+        if (nodes[0].revents & POLLIN) {
+            // no input parser for now ...
+        }
+
+        if (nodes[1].revents & POLLIN) {
+            _read_from_sink();
+        }
+
+        if (nodes[2].revents & POLLIN) {
+            uint64_t res;
+            int rc = read(nodes[2].fd, &res, sizeof(res));
+            if (rc == -1 && errno != EAGAIN) {
+                fprintf(stderr, "[B] read() error on timerfd: %s\n", strerror(errno));
+            } else {
+                _check_connections(&g_servers);
+                _check_connections(&g_clients);
+            }
+        }
+
+        if (nodes[3].revents & POLLIN) {
+            uint64_t res;
+            int rc = read(nodes[3].fd, &res, sizeof(res));
+            if (rc == -1 && errno != EAGAIN) {
+                fprintf(stderr, "[B] read() error on timerfd: %s\n", strerror(errno));
+            } else {
+                _ping();
+            }
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     signal(SIGINT, _int_handler);
+
+    list_init(&g_clients);
+    list_init(&g_servers);
 
     g_lobby = nn_socket(AF_SP, NN_PUB);
     assert(g_lobby >= 0);
@@ -75,10 +262,7 @@ int main(int argc, char **argv)
 
     printf("[B] +++ Broker started (lobby: %s / sink: %s)\n" , BROKER_LOBBY, BROKER_SINK);
 
-    for (;;) {
-        _read_from_sink();
-    }
-
+    _poll();
     _cleanup();
 
     return 0;
--- lobby/c/client.c
+++ lobby/c/client.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <poll.h>
+#include <sys/timerfd.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #include <nanomsg/nn.h>
 #include <nanomsg/pipeline.h>
@@ -12,10 +15,19 @@
 
 static const int g_max_input_length = 256;      // Max number of chars read from the input
 
-char *g_name;
-int g_name_len = 0;
-int g_lobby_sub;
-int g_lobby_sink;
+// client name
+static char *g_name;
+static int g_name_len = 0;
+
+// sockets
+static int g_lobby_sub;
+static int g_lobby_sink;
+
+static bool g_broker_connection = false;
+
+// ongoing requests
+static bool g_list_clients = false;
+static bool g_list_servers = false;
 
 static int _whisper(const char *dest, const char *msg)
 {
@@ -26,6 +38,39 @@ static int _send_to_lobby(const char *msg)
 {
     return BROKER_SEND(g_lobby_sink, BROKER_MSG_FORMAT(g_name, msg));
 }
+
+static int _list_clients()
+{
+    return BROKER_SEND(g_lobby_sink, BROKER_LIST_FORMAT(g_name, BROKER_LIST_CLIENTS));
+}
+
+static int _list_servers()
+{
+    return BROKER_SEND(g_lobby_sink, BROKER_LIST_FORMAT(g_name, BROKER_LIST_SERVERS));
+}
+
+static int _ping()
+{
+    return BROKER_SEND(g_lobby_sink, BROKER_PING_FORMAT(g_name, BROKER_PING_CLIENT));
+}
+
+static ptrdiff_t _strn_trim(char *str, ptrdiff_t max_chars)
+{
+    char *c = str;
+    char *last = c;
+
+    while ((*c != '\0') && ((c - str) < max_chars)) {
+        if (!isspace(*c)) {
+            last = c;
+        }
+
+        c++;
+    }
+
+    *(++last) = '\0';
+
+    return (c - last);
+}                           
 
 static int _get_user_input(char *buffer, size_t buffer_length)
 {
@@ -43,6 +88,13 @@ static int _get_user_input(char *buffer, size_t buffer_length)
         while ((c = getchar()) != '\n' && c != EOF) {}
     }
 
+	// TODO use ncurses to NOT deal with all of this ...
+
+	_strn_trim(buffer, g_max_input_length);
+	if (strlen(buffer) == 0) {
+		return -1;
+	}
+
     return 0;
 }
 
@@ -52,8 +104,14 @@ static void _parse_user_input(const char *input_buffer)
         if (strncmp(input_buffer, "/help", strlen("/help")) == 0) {
             printf("  /help           Print this help message\n");
             printf("  /clients        Show the clients currently in this lobby\n");
+            printf("  /servers        Show the servers availables\n");
             printf("  /msg <u> <m>    Send the message <m> to client <u>\n");
         } else if (strncmp(input_buffer, "/clients", strlen("/clients")) == 0) {
+            _list_clients();
+            g_list_clients = true;
+        } else if (strncmp(input_buffer, "/servers", strlen("/servers")) == 0) {
+            _list_servers();
+            g_list_servers = true;
         } else if (strncmp(input_buffer, "/msg", strlen("/msg")) == 0) {
             char whisp_buffer[BROKER_MAX_MSG_LENGTH];
             strcpy(whisp_buffer, input_buffer);
@@ -80,54 +138,131 @@ static void _parse_user_input(const char *input_buffer)
     }
 }
 
+static void _print_clients(char *list)
+{
+    char *name = strtok(list, BROKER_UNIT_SEPARATOR);
+    while (name) {
+        printf("  c: %s\n", name);
+        name = strtok(NULL, BROKER_UNIT_SEPARATOR);
+    }
+}
+
+static void _print_servers(char *list)
+{
+    char *name = strtok(list, BROKER_UNIT_SEPARATOR);
+    while (name) {
+        printf("  s: %s\n", name);
+        name = strtok(NULL, BROKER_UNIT_SEPARATOR);
+    }
+}
+
 static void _read_from_lobby()
 {
     char *data = NULL;
     int bytes = nn_recv(g_lobby_sub, &data, NN_MSG, 0);
     assert(bytes >= 0);
 
-    /*printf("'%s'\n", data);*/
+    g_broker_connection = true;
 
-    char *user = strtok(data, BROKER_UNIT_SEPARATOR);
-    char *cmd = strtok(NULL, BROKER_UNIT_SEPARATOR);
+    /*if (strncmp(strstr(data, BROKER_RECORD_SEPARATOR) + strlen(BROKER_RECORD_SEPARATOR), BROKER_PING, 4) != 0) printf("'%s'\n", data);*/
 
-    if (strcmp(cmd, BROKER_MSG) == 0) {
-        char *msg = strtok(NULL, BROKER_UNIT_SEPARATOR);
+    char *user = BROKER_FIRST_TOKEN(data);
+    char *cmd = BROKER_NEXT_TOKEN();
 
-        if (strcmp(user, g_name) != 0) {
-            printf("<lobby> %s: %s\n", user, msg);
-        }
+    if (strcmp(cmd, BROKER_PING) == 0) {
+        // ignore
+    } else if (strcmp(cmd, BROKER_MSG) == 0) {
+        char *msg = BROKER_NEXT_TOKEN();
+
+        printf("<lobby> %s: %s\n", user, msg);
     } else if (strcmp(cmd, BROKER_WHISP) == 0) {
-        char *dest = strtok(NULL, BROKER_UNIT_SEPARATOR);
-        char *msg = strtok(NULL, BROKER_UNIT_SEPARATOR);
+        char *dest = BROKER_NEXT_TOKEN();
+        char *msg = BROKER_NEXT_TOKEN();
 
         if (strcmp(dest, g_name) == 0) {
             printf("<whisp> %s: %s\n", user, msg);
         }
+    } else if (strcmp(cmd, BROKER_INFO) == 0) {
+        char *info_type = BROKER_NEXT_TOKEN();
+
+        if (g_list_clients && strcmp(info_type, BROKER_INFO_CLIENTS) == 0) {
+            char *list = BROKER_NEXT_TOKEN();
+            _print_clients(list);
+            g_list_clients = false;
+        } else if (g_list_servers && strcmp(info_type, BROKER_INFO_SERVERS) == 0) {
+            char *list = BROKER_NEXT_TOKEN();
+            _print_servers(list);
+            g_list_servers = false;
+        }
+    } else if (strcmp(cmd, BROKER_SHUTDOWN) == 0) {
+        g_broker_connection = false;
     }
 
     nn_freemsg(data);
 }
 
+static void _input_enable()
+{
+}
+
+static void _input_disable()
+{
+}
+
 static int _poll()
 {
-    struct pollfd nodes[2];
+    struct pollfd nodes[3];
 
     // stdin
-    nodes[0].fd = STDIN_FILENO;
     nodes[0].events = POLLIN;
 
-    // the broker lobby
+    // broker lobby
     size_t fd_size = sizeof(nodes[1].fd);
     assert(nn_getsockopt(g_lobby_sub, NN_SOL_SOCKET, NN_RCVFD, &nodes[1].fd, &fd_size) == 0);
     nodes[1].events = POLLIN;
 
-    for (;;) {
-        char input_buffer[g_max_input_length];
-        int rc = poll(nodes, sizeof(nodes) / sizeof(struct pollfd), -1);
+    // hearthbeat
+    struct itimerspec ts;
+    int tfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+    assert(tfd != -1);
+    nodes[2].events = POLLIN;
 
+    ts.it_interval.tv_sec = BROKER_KEEP_ALIVE_PERIOD;
+    ts.it_interval.tv_nsec = 0;
+    ts.it_value.tv_sec = 0;
+    ts.it_value.tv_nsec = 1;
+
+    char input_buffer[g_max_input_length];
+    bool connected = false;
+    for (;;) {
+        if (connected && !g_broker_connection) {
+            printf("[C] --- Lost connection to lobby, reconnecting ...\n");
+            _input_disable();
+            // disable polling on input
+            nodes[0].fd = 0;
+            // disable hearthbeat timer
+            nodes[2].fd = 0;
+        } else if (!connected && g_broker_connection) {
+            static bool print_once = true;
+            printf("[C] +++ Connected to lobby !\n");
+            if (print_once) {
+                printf("Type '/help' for a list of commands.\n");
+                print_once = false;
+            }
+            _input_enable();
+            // enable polling on input
+            nodes[0].fd = STDIN_FILENO;
+            // enable hearthbeat timer
+            nodes[2].fd = tfd;
+            assert(timerfd_settime(nodes[2].fd, 0, &ts, NULL) == 0);
+        }
+
+        connected = g_broker_connection;
+
+        int rc = poll(nodes, sizeof(nodes) / sizeof(struct pollfd), -1);
         if (rc == -1) {
             fprintf(stderr, "[C] poll() error: %s\n", strerror(errno));
+
             return -1;
         } else if (rc == 0) {
             // timeout
@@ -135,6 +270,7 @@ static int _poll()
         }
 
         if (nodes[0].revents & POLLIN) {
+            memset(input_buffer, 0, sizeof(input_buffer));
             if (_get_user_input(input_buffer, sizeof(input_buffer)) == 0) {
                 _parse_user_input(input_buffer);
             }
@@ -142,6 +278,16 @@ static int _poll()
 
         if (nodes[1].revents & POLLIN) {
             _read_from_lobby();
+        }
+
+        if (nodes[2].revents & POLLIN) {
+            uint64_t res;
+            int rc = read(nodes[2].fd, &res, sizeof(res));
+            if (rc == -1 && errno != EAGAIN) {
+                fprintf(stderr, "[C] read() error on timerfd: %s\n", strerror(errno));
+            } else {
+                _ping();
+            }
         }
     }
 }
@@ -156,7 +302,7 @@ int main(int argc, char **argv)
     g_name = argv[1];
     g_name_len = strlen(g_name);
 
-    printf("[C] ### Connecting as '%s' ...\n", g_name);
+    printf("[C] --- Connecting as '%s' ...\n", g_name);
 
     g_lobby_sub = nn_socket(AF_SP, NN_SUB);
     assert(g_lobby_sub >= 0);
@@ -167,8 +313,6 @@ int main(int argc, char **argv)
     assert(g_lobby_sink >= 0);
     assert(nn_connect(g_lobby_sink, BROKER_SINK) >= 0);
 
-    printf("[C] +++ Connected to broker !\n");
-    printf("Type '/help' for a list of commands.\n");
 
     _poll();
 
