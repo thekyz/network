0ab0f9d2fb5f5bf64c9155b416de37ec8876c3c6
thekyz <thekyz@gmail.com>
2017-01-13 13:56:07 +0100

    Simplified net.c

--- lobby/c/net.c
+++ lobby/c/net.c
@@ -11,13 +11,6 @@
 #define _FORMAT_ARGS2(__u, __t, __a1, __a2)  		"%s%s%s%s%s%s%s", __u, NET_RECORD_SEPARATOR, __t, NET_RECORD_SEPARATOR, __a1, NET_RECORD_SEPARATOR, __a2
 #define _FORMAT_ARGS3(__u, __t, __a1, __a2, __a3)  	"%s%s%s%s%s%s%s%s%s", __u, NET_RECORD_SEPARATOR, __t, NET_RECORD_SEPARATOR, __a1, NET_RECORD_SEPARATOR, __a2, NET_RECORD_SEPARATOR, __a3
 
-#define _MSG_FORMAT(__u, __m)                 _FORMAT_ARGS1(__u, NET_MSG, __m)
-#define _WHISP_FORMAT(__u, __d, __m)          _FORMAT_ARGS2(__u, NET_WHISP, __d, __m)
-#define _LIST_FORMAT(__u, __t)                _FORMAT_ARGS1(__u, NET_LIST, __t)
-#define _INFO_FORMAT(__u, __t, __n, __s)      _FORMAT_ARGS3(__u, NET_INFO, __t, __n, __s)
-#define _PING_FORMAT(__u, __t, __s, __a)      _FORMAT_ARGS3(__u, NET_PING, __t, __s, __a)
-#define _SHUTDOWN_FORMAT(__u)                 _FORMAT_ARGS0(__u, NET_SHUTDOWN)
-
 #define _SEND(__s, __f)      ({                                       \
         char buffer[NET_MAX_MSG_LENGTH];                                 \
         int buffer_size = snprintf(buffer, NET_MAX_MSG_LENGTH - 1, __f); \
@@ -27,36 +20,36 @@
 
 inline int net_whisper(int socket, const char *from, const char *to, const char *msg)
 {
-    return _SEND(socket, _WHISP_FORMAT(from, to, msg));
+    return _SEND(socket, _FORMAT_ARGS2(from, NET_WHISP, to, msg));
 }
 
 inline int net_msg(int socket, const char *from, const char *msg)
 {
-    return _SEND(socket, _MSG_FORMAT(from, msg));
+    return _SEND(socket, _FORMAT_ARGS1(from, NET_MSG, msg));
 }
 
 inline int net_list_clients(int socket, const char *from)
 {
-    return _SEND(socket, _LIST_FORMAT(from, NET_LIST_CLIENTS));
+    return _SEND(socket, _FORMAT_ARGS1(from, NET_LIST, NET_LIST_CLIENTS));
 }
 
 inline int net_list_servers(int socket, const char *from)
 {
-    return _SEND(socket, _LIST_FORMAT(from, NET_LIST_SERVERS));
+    return _SEND(socket, _FORMAT_ARGS1(from, NET_LIST, NET_LIST_SERVERS));
 }
 
 inline int net_info(int socket, const char *from, const char *conn_type, const char *name, const char *state)
 {
-	return _SEND(socket, _INFO_FORMAT(from, conn_type, name, state));
+	return _SEND(socket, _FORMAT_ARGS3(from, NET_INFO, conn_type, name, state));
 }
 
 inline int net_ping(int socket, const char *from, const char *type, const char *state, const char *address)
 {
-    return _SEND(socket, _PING_FORMAT(from, type, state, address));
+    return _SEND(socket, _FORMAT_ARGS3(from, NET_PING, type, state, address));
 }
 
 inline int net_shutdown(int socket, const char *from)
 {
-	return _SEND(socket, _SHUTDOWN_FORMAT(from));
+	return _SEND(socket, _FORMAT_ARGS0(from, NET_SHUTDOWN));
 }
 
